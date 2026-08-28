c9a259f405f17cf2be98bcbd50ff3d0718a9e67f
Kauã Covre Martins <kauacovre222@gmail.com>
2026-08-28 09:27:55 -0300

    Site update
    Atualização no código para hospedar no git hub pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
+/*O arquivo src/main.jsx usa o BrowserRouter. 
+Isso causa um erro 404 (tela branca) se alguém tentar acessar diretamente a URL de um tutorial (ex: /tutorial/windows-family-safety) no GitHub Pages. 
+O HashRouter resolve isso perfeitamente. */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // Alterado aqui
 import App from "./App";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter> {/* Alterado aqui */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
